5fc263185930eda174e6e6c29f470687208520d4
Monte-Sina <sinamohamadi317@gmail.com>
2025-09-11 15:19:03 +0330

    Changed Hero Text

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -7,13 +7,11 @@ function Hero() {
     <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
       <div>
         <h1 className='max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl'>
-          We are changing the way people shop
+          Please read the text below
         </h1>
         <p className='mt-8 max-w-xl text-lg leading-8 text-muted-foreground'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et
-          voluptas saepe in quae voluptate, laborum maiores possimus illum
-          reprehenderit aut delectus veniam cum perferendis unde sint doloremque
-          non nam.
+          This Next.js store application is built and maintained by Monte Sina.
+          For a smooth login and registration process and not getting 403 errors, please ensure your VPN is active and running.
         </p>
         <Button asChild size='lg' className='mt-10'>
           <Link href='/products'>Our Products</Link>
