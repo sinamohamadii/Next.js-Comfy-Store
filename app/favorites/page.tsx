25e30bc410f440be48c4b71e9df2248c41e92a29
Monte-Sina <sinamohamadi317@gmail.com>
2025-09-21 16:14:01 +0330

    Added favorite page and functionality

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,7 +1,17 @@
-const Favorites = () => {
-    return (
-        <div>Favorites</div>
-    )
+import { fetchUserFavorites } from '@/utils/actions';
+import SectionTitle from '@/components/global/SectionTitle';
+import ProductsGrid from '@/components/products/ProductsGrid';
+
+async function FavoritesPage() {
+  const favorites = await fetchUserFavorites();
+  if (favorites.length === 0)
+    return <SectionTitle text='You have no favorites yet.' />;
+  return (
+    <div>
+      <SectionTitle text='Favorites' />
+      <ProductsGrid products={favorites.map((favorite) => favorite.product)} />
+    </div>
+  );
 }
 
-export default Favorites
+export default FavoritesPage;
--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,10 +1,12 @@
-import { FaHeart } from 'react-icons/fa';
-import { Button } from '@/components/ui/button';
-function FavoriteToggleButton({ productId }: { productId: string }) {
-  return (
-    <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
-      <FaHeart />
-    </Button>
-  );
+import { auth } from '@clerk/nextjs/server';
+import { CardSignInButton } from '../form/Buttons';
+import { fetchFavoriteId } from '@/utils/actions';
+import FavoriteToggleForm from './FavoriteToggleForm';
+async function FavoriteToggleButton({ productId }: { productId: string }) {
+  const { userId } = auth();
+  if (!userId) return <CardSignInButton />;
+  const favoriteId = await fetchFavoriteId({ productId });
+
+  return <FavoriteToggleForm favoriteId={favoriteId} productId={productId} />;
 }
 export default FavoriteToggleButton;
